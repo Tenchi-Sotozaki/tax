3447b3fef791477a9f8a8e1ee75ceec5c0f35d09
亀井冬馬 <touma_kamei@everyit.co.jp>
2026-05-12 11:28:14 +0900

    ユーザー編集時のパスワード入力の必須を外す 登録時と編集時の登録メソッドの分岐修正

--- src/main/java/jp/lg/asp/accommodation/dto/UserForm.java
+++ src/main/java/jp/lg/asp/accommodation/dto/UserForm.java
@@ -10,29 +10,32 @@ import lombok.Data;
 @Data
 public class UserForm {
 
+	public interface OnCreate {}
+	public interface OnUpdate {}
+
 	private String originalId;
 
-	@NotBlank(message = "IDは必須です")
+	@NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "IDは必須です")
 	@Size(max = 100)
 	private String id;
 
 	private String currentPassword;
 
-	@NotBlank(message = "パスワードは必須です")
+	@NotBlank(groups = OnCreate.class, message = "パスワードは必須です")
 	@Size(max = 64, message = "パスワードは64文字以内で入力してください")
 	private String password;
 
 	private String passwordConfirm;
 
-	@NotBlank(message = "氏名は必須です")
+	@NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "氏名は必須です")
 	@Size(max = 200)
 	private String name;
 
-	@NotBlank(message = "ふりがなは必須です")
+	@NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "ふりがなは必須です")
 	@Size(max = 200)
 	private String nameKana;
 
-	@NotBlank(message = "部署は必須です")
+	@NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "部署は必須です")
 	@Size(max = 200)
 	private String busho;
 
