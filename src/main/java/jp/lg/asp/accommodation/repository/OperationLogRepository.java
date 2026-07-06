7797de309fad86dcf6a4047c620a82fecdb1667e
nobuyuki saito <nobuyuki_saito@everyit.co.jp>
2026-07-06 11:17:55 +0900

    操作ログが検索できない問題を修正

--- src/main/java/jp/lg/asp/accommodation/repository/OperationLogRepository.java
+++ src/main/java/jp/lg/asp/accommodation/repository/OperationLogRepository.java
@@ -19,12 +19,12 @@ public interface OperationLogRepository extends JpaRepository<OperationLog, Oper
 	@Query("""
 			SELECT o FROM OperationLog o
 			WHERE o.jichitaiCd = :jichitaiCd
-			AND (:screenId IS NULL OR o.screenId = :screenId)
-			AND (:sousa IS NULL OR o.sousa LIKE CONCAT('%', :sousa, '%'))
-			AND (:opeUser IS NULL OR o.opeUser LIKE CONCAT(:opeUser, '%'))
-			AND (:opeDtFrom IS NULL OR o.opeDt >= TO_DATE(:opeDtFrom, 'yyyy/MM/dd HH:mm:ss'))
-			AND (:opeDtTo IS NULL OR o.opeDt <= TO_DATE(:opeDtTo, 'yyyy/MM/dd HH:mm:ss'))
-			AND (:param IS NULL OR o.param LIKE CONCAT('%', :param, '%'))
+			AND (:screenId IS NULL OR :screenId = '' OR o.screenId = :screenId)
+			AND (:sousa IS NULL OR :sousa = '' OR o.sousa LIKE CONCAT('%', :sousa, '%'))
+			AND (:opeUser IS NULL OR :opeUser = '' OR o.opeUser LIKE CONCAT(:opeUser, '%'))
+			AND (:opeDtFrom IS NULL OR :opeDtFrom = '' OR o.opeDt >= TO_DATE(:opeDtFrom, 'yyyy/MM/dd HH:mm:ss'))
+			AND (:opeDtTo IS NULL OR :opeDtTo = '' OR o.opeDt <= TO_DATE(:opeDtTo, 'yyyy/MM/dd HH:mm:ss'))
+			AND (:param IS NULL OR :param = '' OR o.param LIKE CONCAT('%', :param, '%'))
 			ORDER BY o.opeDt DESC
 			""")
 	List<OperationLog> findByConditions(
