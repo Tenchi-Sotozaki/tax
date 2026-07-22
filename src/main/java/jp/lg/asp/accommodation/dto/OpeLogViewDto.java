ef8ca3e55df76f975602f223aff65c15bda4faba
nobuyuki saito <nobuyuki_saito@everyit.co.jp>
2026-07-22 10:36:56 +0900

    操作ログ、帳票ログで日時指定がエラーになる問題の対処

--- src/main/java/jp/lg/asp/accommodation/dto/OpeLogViewDto.java
+++ src/main/java/jp/lg/asp/accommodation/dto/OpeLogViewDto.java
@@ -2,8 +2,6 @@ package jp.lg.asp.accommodation.dto;
 
 import java.time.LocalDateTime;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.Data;
 
 /**
@@ -24,11 +22,9 @@ public class OpeLogViewDto {
 	private String opeUser;
 
 	/** 操作日時 FROM */
-	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
 	private String opeDtFrom;
 
 	/** 操作日時 TO */
-	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
 	private String opeDtTo;
 
 	/** 任意項目／パラメータ（部分一致） */
