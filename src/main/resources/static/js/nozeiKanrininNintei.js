cf03fa6f31267c7287b78af3ffbfea577b048c21
Kuboichi_Atsumu <atsumu_kuboichi@everyit.co.jp>
2026-08-04 09:44:13 +0900

    納税管理人選任免除認定(不認定)通知書の修正

--- src/main/resources/static/js/nozeiKanrininNintei.js
+++ src/main/resources/static/js/nozeiKanrininNintei.js
@@ -7,3 +7,22 @@ function validateForm() {
     }
     return true;
 }
+
+// 区分（認定/不認定）の変更に応じて「不認定の理由」の有効/無効を切り替える
+function toggleBikoState() {
+    const ninteiSelect = document.getElementById('nintei');
+    const bikoTextarea = document.getElementById('biko');
+
+    if (ninteiSelect.value === '認定') { // 認定
+		// 認定に変更された場合は理由をクリア
+        bikoTextarea.value = '';
+        bikoTextarea.disabled = true;
+    } else { // 不認定
+        bikoTextarea.disabled = false;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	
+    toggleBikoState();
+});
