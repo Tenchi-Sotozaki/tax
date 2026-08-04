a90fc18894dd1ba280e1ad7e0bba0b77108c49f6
evryit-kimura <sousuke_kimura@everyit.co.jp>
2026-08-04 15:13:36 +0900

    賦課方式照会の修正

--- src/main/resources/static/js/zeiritsuDaicho.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btnView').addEventListener('click', function () {
-    const selected = document.querySelector('input[name="selectedRow"]:checked');
-    if (!selected) {
-        alert('照会するデータを選択してください。');
-        return;
-    }
-    location.href = document.getElementById('btnView').dataset.viewBase + selected.value;
-});
