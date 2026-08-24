6c7c13a4923ce182d6bdb676274717c80cb5a14d
Kuboichi_Atsumu <atsumu_kuboichi@everyit.co.jp>
2026-08-24 11:29:31 +0900

    ボタン修正

--- src/main/java/jp/lg/asp/accommodation/service/impl/GassanNonyuTsuchiServiceImpl.java
+++ src/main/java/jp/lg/asp/accommodation/service/impl/GassanNonyuTsuchiServiceImpl.java
@@ -100,7 +100,7 @@ public class GassanNonyuTsuchiServiceImpl implements GassanNonyuTsuchiService {
 			// 納入期限を取得
 			Jichitai jichitaiInfo = reportsCommonService.getJichitaiInfo();
 			if (jichitaiInfo != null && tekiyoStYmd != null) {
-				int nendoStMonth = Integer.parseInt(jichitaiInfo.getNendoStMonth());
+				int nendoStMonth = Integer.parseInt(jichitaiInfo.getNendoStMonth().trim());
 				int targetMonth = tekiyoStYmd.getMonthValue();
 				int ki = ((targetMonth - nendoStMonth + 12) % 12) + 1;
 				int nendo = tekiyoStYmd.getYear();
