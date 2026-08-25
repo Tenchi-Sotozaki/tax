8a35654cbadc93a35dde7695fa5d4d8b1f516b1d
Kuboichi_Atsumu <atsumu_kuboichi@everyit.co.jp>
2026-08-25 17:28:36 +0900

    Repositoryに自治体マスタへユーザー名を登録する処理を追加

--- src/main/java/jp/lg/asp/accommodation/repository/JichitaiRepository.java
+++ src/main/java/jp/lg/asp/accommodation/repository/JichitaiRepository.java
@@ -3,6 +3,9 @@ package jp.lg.asp.accommodation.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import jp.lg.asp.accommodation.entity.Jichitai;
@@ -18,4 +21,14 @@ public interface JichitaiRepository extends JpaRepository<Jichitai, String> {
 	 * @return 該当する自治体。存在しない場合は empty
 	 */
 	Optional<Jichitai> findFirstByParam(String param);
+	
+	/**
+     * 自治体コードを指定して user_name を更新する
+     */
+    @Modifying
+    @Query("UPDATE Jichitai j SET j.userName = :userName WHERE j.jichitaiCd = :jichitaiCd")
+    int updateUserNameByJichitaiCd(
+        @Param("jichitaiCd") String jichitaiCd, 
+        @Param("userName") String userName
+    );
 }
