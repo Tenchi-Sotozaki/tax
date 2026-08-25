c65df16ec72363a5c8004b71044aa9b114bd402f
Kuboichi_Atsumu <atsumu_kuboichi@everyit.co.jp>
2026-08-25 15:44:50 +0900

    年号を和暦化

--- src/main/java/jp/lg/asp/accommodation/service/impl/NozeiKanrininNinteiReportsServiceImpl.java
+++ src/main/java/jp/lg/asp/accommodation/service/impl/NozeiKanrininNinteiReportsServiceImpl.java
@@ -2,16 +2,19 @@ package jp.lg.asp.accommodation.service.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.chrono.JapaneseChronology;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import jp.lg.asp.accommodation.dto.NozeiKanrininNinteiDto;
+import jp.lg.asp.accommodation.dto.NozeiKanrininNinteiReportsDto;
 import jp.lg.asp.accommodation.service.NozeiKanrininNinteiReportsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +25,7 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 /**
  * 納税管理人選任免除認定（不認定）通知書PDF生成 Service実装
@@ -52,25 +55,35 @@ public class NozeiKanrininNinteiReportsServiceImpl implements NozeiKanrininNinte
 	        parameters.put("net.sf.jasperreports.default.pdf.encoding", "Identity-H");
 	        parameters.put("net.sf.jasperreports.default.pdf.embedded", "true");
 
-	        if (dto.getHakkoYmd() != null) {
-	            parameters.put("hakkoYmd", dto.getHakkoYmd().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日")));
-	        } else {
-	            parameters.put("hakkoYmd", "");
-	        }
+			// 発行日
+			if (dto.getHakkoYmd() != null) {
+				// 和暦用のフォーマッタを作成
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("Gy年M月dd日", Locale.JAPANESE)
+						.withChronology(JapaneseChronology.INSTANCE);
+
+				String strDate = dto.getHakkoYmd().format(formatter);
+				parameters.put("hakkoYmd", strDate);
+			} else {
+				parameters.put("hakkoYmd", "");
+			}
+			
 	        parameters.put("jorei", dto.getJorei() != null ? dto.getJorei() : "");
 	        parameters.put("city", dto.getCityName() != null ? dto.getCityName() : "");
 	        parameters.put("biko", dto.getBiko() != null ? dto.getBiko() : "");
 	        parameters.put("nintei", dto.getNintei() != null ? dto.getNintei() : "認定");
 
-	        Map<String, Object> row = new HashMap<>();
-	        row.put("jusho", dto.getTokuJusho() != null ? dto.getTokuJusho() : "");
-	        row.put("name", dto.getTokuName() != null ? dto.getTokuName() : "");
-	        row.put("shisetsu_jusho", dto.getShisetsuJusho() != null ? dto.getShisetsuJusho() : "");
-	        row.put("shisetsu_name", dto.getShisetsuName() != null ? dto.getShisetsuName() : "");
-	        row.put("koin", dto.getKoin() != null && dto.getKoin().length > 0 ? dto.getKoin() : null);
-
-	        List<Map<String, ?>> dataSourceList = Arrays.asList(row);
-	        JRDataSource dataSource = new JRMapCollectionDataSource(dataSourceList);
+	        // field設定
+	        NozeiKanrininNinteiReportsDto reportDto = new NozeiKanrininNinteiReportsDto();
+	        reportDto.setYubin(dto.getTokuYubin() != null ? dto.getTokuYubin() : "");
+	        reportDto.setJusho(dto.getTokuJusho() != null ? dto.getTokuJusho() : "");
+	        reportDto.setName(dto.getTokuName() != null ? dto.getTokuName() : "");
+	        reportDto.setShisetsuYubin(dto.getShisetsuYubin() != null ? dto.getShisetsuYubin() : "");
+	        reportDto.setShisetsuJusho(dto.getShisetsuJusho() != null ? dto.getShisetsuJusho() : "");
+	        reportDto.setShisetsuName(dto.getShisetsuName() != null ? dto.getShisetsuName() : "");
+	        reportDto.setKoin(dto.getKoin() != null && dto.getKoin().length > 0 ? dto.getKoin() : null);
+	       
+	        List<NozeiKanrininNinteiReportsDto> dataSourceList = Arrays.asList(reportDto);
+	        JRDataSource dataSource = new JRBeanCollectionDataSource(dataSourceList);
 
 	        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 	        return JasperExportManager.exportReportToPdf(jasperPrint);
