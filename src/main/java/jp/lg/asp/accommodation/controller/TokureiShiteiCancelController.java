d8cccb1129cfa7b70e36bba66c8e8bbc4c1cfca8
Kuboichi_Atsumu <atsumu_kuboichi@everyit.co.jp>
2026-09-01 17:19:01 +0900

    テストエラー修正

--- src/main/java/jp/lg/asp/accommodation/controller/TokureiShiteiCancelController.java
+++ src/main/java/jp/lg/asp/accommodation/controller/TokureiShiteiCancelController.java
@@ -60,14 +60,12 @@ public class TokureiShiteiCancelController {
 			return "tokugimu/tTokugimuReport";
 		}
 		
-		TokureiShiteiCancelDto dto = new TokureiShiteiCancelDto();
-
 		TokureiShiteiDto shiteiDto = tokureiShiteiService.getTokugimuInfo(shiteiNo);
 		if (shiteiDto == null) {
 			throw new IllegalArgumentException("特例情報が存在しません。");
 		}
 		
-		dto = convertToCancel(shiteiDto);
+		TokureiShiteiCancelDto dto = convertToCancel(shiteiDto);
 
 		if (dto.getHakkoYmd() == null) {
 			dto.setHakkoYmd(LocalDate.now());
